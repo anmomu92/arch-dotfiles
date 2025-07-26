c960539985b64d401cda4488112d152b6e2b1c43
Antonio Morán Muñoz <anmomu92@gmail.com>
2025-07-26 23:29:30 +0200

    Dock optional

--- .config/amm92/scripts/switch-theme.sh
+++ .config/amm92/scripts/switch-theme.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+LOG_PATH="$HOME/.local/log"
+LOG_FILE="$LOG_PATH/switch-theme.log"
 STATE_PATH="$HOME/.local/state/amm92"
+DOCK_STATE=$(<"$STATE_PATH/dock")
 
 if [ ! -d "$STATE_PATH" ]; then 
 	mkdir -p "$STATE_PATH"
-	echo "claro" > "$STATE_PATH/system-theme"
+	echo "light" > "$STATE_PATH/system-theme"
 elif [ ! -f "$STATE_PATH/system-theme" ]; then
-	echo "claro" > "$STATE_PATH/system-theme"
+	echo "light" > "$STATE_PATH/system-theme"
 fi
 
 CURRENT_THEME=$(<"$STATE_PATH/system-theme")
+
+echo "SWITCH THEME SCRIPT LOG" | tee $LOG_FILE 
+echo "-----------------------" | tee -a $LOG_FILE 
+echo "" | tee -a $LOG_FILE 
+echo "Current theme is $CURRENT_THEME" | tee -a $LOG_FILE 
 
 # We switch the theme
 if [ "${CURRENT_THEME}" == "dark" ]; then
@@ -29,14 +37,22 @@ fi
 
 # Qutebrowser
 if [ $(pgrep qutebrowser) ]; then
+	echo "Qutebrowser theme changed" | tee -a $LOG_FILE 
 	qutebrowser ":config-cycle colors.webpage.darkmode.enabled false true"
 fi
 
-# Dock
 nwg-look -a     # Apply changes
 nwg-look -x     # Export changes
-$HOME/.config/nwg-dock-hyprland/launch.sh
+
+# Dock
+if [ "$DOCK_STATE" == "active" ]; then 
+	echo "Dock theme changed" | tee -a $LOG_FILE 
+	$HOME/.config/nwg-dock-hyprland/launch.sh
+fi
     
 # We notify the change 
 NEW_THEME=$(<"$STATE_PATH/system-theme")
 notify-send "Theme switched to $NEW_THEME"
+
+echo "" | tee -a $LOG_FILE 
+echo "New theme is $CURRENT_THEME" | tee -a $LOG_FILE 
