2fc610b200a7227835884e75fb5b7fd3165b21bd
Antonio Morán Muñoz <anmomu92@gmail.com>
2026-02-14 09:44:03 +0100

    Fixed script path

--- .config/amm92/settings/installupdates.sh
+++ .config/amm92/settings/installupdates.sh
@@ -1,1 +1,1 @@
-$(cat ~/.local/state/amm92/terminal) --class dotfiles-floating -e ~/.config/amm92/scripts/installupdates.sh
+$(cat ~/.local/state/amm92/terminal) --class dotfiles-floating -e ~/.local/scripts/installupdates.sh
