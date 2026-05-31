93b3e9c9c592bf879a03f5bd335230c67488f411
Antonio Morán Muñoz <anmomu92@gmail.com>
2026-05-31 16:22:42 +0200

    update: hyprland inputs and monitors file written in lua

---
+++ .config/hypr/inputs.lua
@@ -0,0 +1,19 @@
+hl.config({
+	input = {
+		kb_layout = "es",
+		kb_variant = "",
+		kb_model = "",
+		kb_options = "",
+		kb_rules = "",
+
+		numlock_by_default = true,
+		mouse_refocus = false,
+
+		follow_mouse = 1,
+		sensitivity = 0,
+
+		touchpad = {
+			natural_scroll = true,
+		},
+	}
+})
