9aa4c50b863636e98b91d2e80ffddb75a5a8175f
Antonio Morán Muñoz <anmomu92@gmail.com>
2026-05-31 16:21:47 +0200

    update: hyprland main config file written in lua

---
+++ .config/hypr/hyprland.lua
@@ -0,0 +1,20 @@
+-- monitors
+require("monitors")
+
+-- input
+require("inputs")
+
+-- autostart
+require("conf.autostart")
+
+-- configuration
+require("conf.amm92")
+require("conf.animation")
+require("conf.decoration")
+require("conf.environment")
+require("conf.keybinding")
+require("conf.layout")
+require("conf.misc")
+require("conf.window")
+require("conf.windowrule")
+require("conf.workspace")
